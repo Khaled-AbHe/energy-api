cb6e7cc9a9c60f07d3435b3d8f324808d4fd0b2b
root <root@cod-1-abouhechmehkha.lan>
2026-09-04 19:15:37 +0000

    Fait du progres

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,12 +1,25 @@
 import { Controller, Get } from '@nestjs/common';
 import { HealthService } from './health.service';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 
+@ApiTags("Health")
 @Controller('health')
 export class HealthController {
 
   constructor(private healthService: HealthService) {}
 
   @Get()
+  @ApiOperation({
+    summary: "Chercher le statut de l'api",
+    description: 'Chercher un bâtiment à partir de la collection courante par son ID.',
+  })
+  @ApiOkResponse({
+    description: 'Détails de santé.',
+  })
   getHello(): any {
     return this.healthService.getHealthStatus();
   }
--- src/rooms/rooms.controller.ts
+++ src/rooms/rooms.controller.ts
@@ -2,32 +2,87 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode } from '@ne
 import { RoomsService } from './rooms.service';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
-import { ApiBadGatewayResponse, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiParam,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
 
+  // Create one room
   @Post()
+  @ApiOperation({
+    summary: 'Créer une chambre',
+    description: 'Ajoute une chambre à la collection courante.',
+  })
+  @ApiCreatedResponse({
+    description: 'Chambre créé.',
+    headers: {
+      Location: {
+        description: 'URI de la nouvelle ressource',
+        schema: { type: 'string' },
+      },
+    },
+  })
   create(@Body() createRoomDto: CreateRoomDto) {
     return this.roomsService.create(createRoomDto);
   }
 
+  // Get all rooms
   @Get()
+  @ApiOperation({
+    summary: 'Chercher le collection de chambres',
+    description: 'Chercher toutes les chambres dans la collection.',
+  })
+  @ApiOkResponse({
+    description: 'Chambres trouvé.',
+  })
   findAll() {
     return this.roomsService.findAll();
   }
 
+  // Get all rooms
   @Get(':id')
+  @ApiOperation({
+    summary: 'Chercher une chambre',
+    description: 'Cherche une chambre à partir de la collection courante par son ID.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identifiant UUID de la chambre',
+    format: 'uuid',
+  })
+  @ApiOkResponse({
+    description: 'Chambre trouvé.',
+  })
   findOne(@Param('id') id: string) {
     return this.roomsService.findOne(id);
   }
 
+  // Update one room
   @Patch(':id')
+  @ApiOperation({
+    summary: 'Mettre une chambre à jour',
+    description: 'Cherche une chambre à partir de la collection courante par son ID.',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Identifiant UUID de la chambre',
+    format: 'uuid',
+  })
+  @ApiOkResponse({
+    description: 'Chambre trouvé.',
+  })
   update(@Param('id') id: string, @Body() updateRoomDto: UpdateRoomDto) {
     return this.roomsService.update(id, updateRoomDto);
   }
 
+  // Delete one room
   @Delete(':id')
   @HttpCode(204)
   remove(@Param('id') id: string) {
